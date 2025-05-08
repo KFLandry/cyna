37ffd4bca54073b1ec33097cb5ba02ad9a7fa518
GDevWeb <zodiaquestar@gmail.com>
2025-05-08 16:31:14 +0200

    ✅ Fix BE crash sur création d'adresse utilisateur (500)

--- auth-users/src/main/java/com/cyna/auth_users/AuthUsersApplication.java
+++ auth-users/src/main/java/com/cyna/auth_users/AuthUsersApplication.java
@@ -3,11 +3,10 @@ package com.cyna.auth_users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.cyna.auth_users")
 public class AuthUsersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthUsersApplication.class, args);
 	}
-
 }
