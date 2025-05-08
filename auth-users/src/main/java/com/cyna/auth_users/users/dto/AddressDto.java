8e00e82dd53441bedf69f6c7f370b4d60f2965b3
GDevWeb <zodiaquestar@gmail.com>
2025-05-08 20:07:58 +0200

    feat(address): implémentation complète de la gestion des adresses utilisateur

--- auth-users/src/main/java/com/cyna/auth_users/users/dto/AddressDto.java
+++ auth-users/src/main/java/com/cyna/auth_users/users/dto/AddressDto.java
@@ -1,5 +1,6 @@
 package com.cyna.auth_users.users.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -7,19 +8,22 @@ import lombok.Data;
 public class AddressDto {
     private Long id;
 
+    @NotBlank(message = "Le nom de l'adresse est obligatoire")
     private String name;
 
-    @Pattern(regexp = "^(0[1-9]|[1-8]\\d|9[0-5])\\d{3}$",
-            message = "Le code postal doit comporter 5 chiffres et être valide")
+    @Pattern(
+            regexp = "^(0[1-9]|[1-8]\\d|9[0-5])\\d{3}$",
+            message = "Le code postal doit comporter 5 chiffres et être valide"
+    )
     private String postcode;
 
+    @NotBlank(message = "La ville est obligatoire")
     private String city;
 
+    @NotBlank(message = "Le pays est obligatoire")
     private String country;
 
     private Long userId;
-
     private String customerId;
-
     private String url;
 }
