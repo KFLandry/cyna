7f1b8431a6054ffd17dd913c2e694cd7778302aa
KFLandry <wilfried-landry.kankeufokou@worldline.com>
2025-06-06 10:32:52 +0200

    feat : add PricingModel enum for subscription pricing options

---
+++ subscriptions/src/main/java/com/cyna/subscriptions/models/PricingModel.java
@@ -0,0 +1,10 @@
+package com.cyna.subscriptions.models;
+
+public enum PricingModel {
+    PER_MONTH_PER_USER,
+    PER_YEAR_PER_USER,
+    PER_MONTH_PER_DEVICE,
+    PER_YEAR_PER_DEVICE,
+    PAY_AS_YOU_GO,
+    ONE_TIME
+}
